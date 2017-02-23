4aa9aa8a0080ca66ef2efbdd2d287cd7c7800e1f
Renee <renehuang0917@gmail.com>
2017-02-23 10:05:06 -0500

    week6 added

---
+++ week6/exampleClass/exampleClass.ksh
@@ -0,0 +1,6 @@
+#!/bin/ksh
+
+BASEDIR=/mnt/nas/shared/progQuantCompFinance/lecture7/exampleClass
+PROG=exampleClass.out
+
+$BASEDIR/run.ksh $BASEDIR $PROG
---
+++ week6/exampleDataStructure/myFunc2.cpp
@@ -0,0 +1,11 @@
+#include "myStructures.h"
+#include "functions.h"
+
+void myFunc2(dataAttributes myData)
+{
+
+	myData.myMarket.market = "Bund";
+	myData.myMarket.symbolName= "RX";
+	myData.myMarket.exchangeName = "Eurex";
+
+}
